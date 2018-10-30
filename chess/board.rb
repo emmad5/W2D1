495f336d312c1a388ee2725db89f393cdbb425dc
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-10-29 17:53:22 -0700

    Add #populate to the board.rb

--- chess/board.rb
+++ chess/board.rb
@@ -3,27 +3,29 @@ class Board
   attr_accessor :board
   def initialize
     @board = Array.new(8) { Array.new(8) }
-    @board[0,3], @board[7,3] = Piece.new("Queen")
-    @board[0,4], @board[7,4] = Piece.new("King")
-    @board[0,0], @board[0,7], @board[7,0], @board[7,7] = Piece.new("Rook")
-    @board[0,1], @board[0,6], @board[7,1], @board[7,6] = Piece.new("Knight")
-    @board[0,2], @board[0,5], @board[7,2], @board[7,5] = Piece.new("Bishop")
-    (0..7).each do |i|
-      @board[1, i] = Piece.new("Pawn")
-    end
-    (0..7).each do |i|
-      @board[6, i] = Piece.new("Pawn")
-    end
-      
+    populate
   end
   
+  def populate  
+    self[[0,3]], self[[7,3]] = Piece.new("Queen"), Piece.new("Queen")
+    self[[0,4]], self[[7,4]] = Piece.new("King"), Piece.new("King")
+    self[[0,0]], self[[0,7]], self[[7,0]], self[[7,7]] = Piece.new("Rook"), Piece.new("Rook"), Piece.new("Rook"), Piece.new("Rook")
+    self[[0,1]], self[[0,6]], self[[7,1]], self[[7,6]] = Piece.new("Knight"), Piece.new("Knight"), Piece.new("Knight"), Piece.new("Knight") 
+    self[[0,2]], self[[0,5]], self[[7,2]], self[[7,5]] = Piece.new("Bishop"), Piece.new("Bishop"), Piece.new("Bishop"), Piece.new("Bishop")
+    (0..7).each do |i|
+      self[[1, i]] = Piece.new("Pawn")
+    end
+    (0..7).each do |i|
+      self[[6, i]] = Piece.new("Pawn")
+    end  
+  end
   
   def move_piece(start_pos, end_pos)
-    raise "There is no piece at #{start_pos}" if @board[start_pos].nil?
-    raise "The piece cannot move to #{end_pos}" unless @board[end_pos].nil?
-    piece = @board[start_pos]
-    @board[end_pos] = piece
-    @board[start_pos] = nil
+    raise "There is no piece at #{start_pos}" if self[start_pos].nil?
+    raise "The piece cannot move to #{end_pos}" unless self[end_pos].nil?
+    piece = self[start_pos]
+    self[end_pos] = piece
+    self[start_pos] = nil
   end
   
   def [](pos)
@@ -32,6 +34,7 @@ class Board
   end
   
   def []=(pos, mark)
-    @board[pos] = mark
+    row, col = pos
+    @board[row][col] = mark
   end
 end
